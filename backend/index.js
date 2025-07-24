8809b0777e94c5ee33e6f9ac20a2b3f061503e12
Balram Dhakad <bdhakad886@gmail.com>
2025-07-25 01:35:34 +0530

    Final deploy ready url changed

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5000"],
+    origin: ["https://last-minute-scsit.vercel.app", "https://lastminutescsit-api.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
