2116f9c8bc19d1fd3657da022998b137f48cf1e4
Balram Dhakad <bdhakad886@gmail.com>
2025-07-26 16:22:07 +0530

    cors fix 2

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-    origin: ["https://last-minute-scsit.vercel.app", "https://lastminutescsit-api.vercel.app"],
+    origin: "https://last-minute-scsit.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
