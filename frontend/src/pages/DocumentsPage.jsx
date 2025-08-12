5ed5d2ffd95bc395b54dda41a153e881d37639e6
Balram Dhakad <bdhakad886@gmail.com>
2025-08-12 14:24:21 +0530

    Final Changes 1.2

--- frontend/src/pages/DocumentsPage.jsx
+++ frontend/src/pages/DocumentsPage.jsx
@@ -348,7 +348,7 @@ const DocumentsPage = () => {
                                             No {categories.find(cat => cat.value === selectedCategory)?.label.toLowerCase()} available for this subject yet.
                                         </div>
                                     )}
-                                    {showAll > 6 && <span className="absolute bottom-2 right-4 cursor-pointer text-blue-400 text-sm" onClick={() => setShowAll(showAll == 6 ? null : 6)}>{showAll == 6 ? "Show All" : "Show Less"}</span>}
+                                    {filteredPapers.length > 6 && <span className="absolute bottom-2 right-4 cursor-pointer text-blue-400 text-sm" onClick={() => setShowAll(showAll == 6 ? null : 6)}>{showAll == 6 ? "Show All" : "Show Less"}</span>}
                                 </div>
                             </motion.div>
                         );
