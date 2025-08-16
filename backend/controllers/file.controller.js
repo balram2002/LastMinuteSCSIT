4e31d5a480908665a5467facc1ab51e557b1f387
Balram Dhakad <bdhakad886@gmail.com>
2025-08-16 20:10:25 +0530

    Final Version 2.8

--- backend/controllers/file.controller.js
+++ backend/controllers/file.controller.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import { File } from '../models/FileModel.js';
 import axios from 'axios';
 
-const API_URL = "https://lastminutescsit-api.vercel.app";
+const API_URL = "https://lastminutescsit-api.vercel.app"; 
 // const API_URL = "http://localhost:5000";
 
 export const uploadFile = async (req, res) => {
